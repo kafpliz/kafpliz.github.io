4a84482cc5b75a7197f1b6837f3c26d183ad171b
kafpliz <91686116+kafpliz@users.noreply.github.com>
2022-03-12 19:59:00 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+ <head>
+    <meta charset="UTF-8">
+    <title>Лучшие аниме для просмотра дома!</title>
+<!--Для поисковиков -->
+ <meta http-equiv="Content-Type" content="text/html">
+<meta name="description" content="Узнать лучшие аниме для просмотра - с русской озвучкой и классной рисовкой. "/>
+    <meta name="keywords" content="лучшие, аниме, для, просмотра, узнать, с, русской, озвучкой, и, классной, рисовкой, "/>
+
+
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta content="width=device-width, initial-scale=1" name="viewport" />
+<meta name="robots" content="noindex, nofollow" />
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+        
+
+
+
+    <link rel='icon' href='http://over.sw/favicon.ico' type='image/x-icon'/>
+    <link rel='shortcut icon' href='http://over.sw/favicon.ico' type='image/xicon'/>
+    <link rel="apple-touch-icon" href="http://over.sw/180.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="http://over.sw/60.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="http://over.sw/76.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="http://over.sw/120.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="http://over.sw/152.png">
+
+
+<style type="text/css">
+
+    body { margin:0;padding:0;background:url(/icon/image/fon1.jpg) no-repeat; background-size: 100%;background-attachment: fixed; }
+.user_menu{color: white;background:  #333;width: 280px;float: right;min-height: 50vh;margin-right: 200px; border: 2px solid  #0eb092;border-radius: 10px;}
+div.wrapper {background-color: #333;color: #9198a3;margin-left: 26%;margin-right: 26%;min-height: 100vh; border: 2px solid #0eb092; border-radius: 5px; z-index: 1; } 
+p.logo{text-align: center;}
+nav.search{ background-color:#0f1114; height:39px ;}
+a{text-decoration: none;}
+input#in{height: 30px; border: none; outline:none;padding: 4px; margin-left: 6px; margin-top: 6px;}
+li, #header{margin: 6px;}
+div.header{display: inline; float: right; margin-top: 13px; background-color: #17191c;padding: 4px; padding-right: 30px; }
+a#soc{text-decoration: none; color: whitesmoke; }
+a#soc:hover { color: red;}
+div#drop:hover .dropcontent {display: block;}
+div.dropcontent{display: none; height:421; margin-right: 36%;margin-left: -8%; position: absolute;background:  #17191c; z-index: 1;}
+div.dropcontent a{color: white; text-decoration: none;display: inline-block;width: 81px; height: auto; }
+li{list-style: none; width: auto;}
+ul{margin: 0%; padding: 0%;}
+ul.leftdropmenu{ column-count:2;column-gap: 15px; padding-top: 5px;margin-left: 36%; padding-right: 6px;padding-left:6px ;column-width: 120px; font-size: 15px; float: right;}
+ul.pick{column-count:1; padding-top: 5px; margin-left: -130%; padding: 5px;  font-size: 15px; float: left; height: 395.5px; width:150px ;background: #5f615f; font-size:18px} 
+hr{background-color: #0eb092; text-align: center; border: none; height: 2px;}
+.otdel{color: white ;  color: white ; margin-top: 50px; text-align: center;}
+div.double_wrapper  {color: white;}
+p.z1{margin-left: 15%; margin-right: 15%; color: white; background:url(/icon//image/news.jpeg) no-repeat;background-size: 100%; border: 2px solid #5e676b; border-radius: 4px; height: 100px;text-align: center; padding-top: 50px;}
+p.z2{margin-left: 15%; margin-right: 15%; color: white; background:url(/icon//image/anime.jpeg) no-repeat;background-size: 100%; border: 2px solid #5e676b; border-radius: 4px; height: 100px;text-align: center; padding-top: 50px; }
+p.z3{margin-left: 15%; margin-right: 15%; color: white; background:url(/icon//image/film.jpeg) no-repeat;background-size: 100%; border: 2px solid #5e676b; border-radius: 4px; height: 100px;text-align: center; padding-top: 50px;}
+p.z4{margin-left: 15%; margin-right: 15%; color: white; background-image:url() ; border: 2px solid #5e676b; border-radius: 4px; height: 100px;text-align: center; padding-top: 50px;}
+p.z5{margin-left: 15%; margin-right: 15%; color: white; background-image:url() ; border: 2px solid #5e676b; border-radius: 4px; height: 100px;text-align: center; padding-top: 50px;}
+
+li#w:hover{color: #00197d;}
+a#r:hover {color: #c48b0e;}
+div.button_click_hidden_menu{display: none;}
+a#c {color: whitesmoke; background-color: rgba(23, 25, 28, .9); font-size: 25px; border: 1px solid rgba(23, 25, 28, .9 ; border-radius: 6px;}
+
+div.dschat{margin-left: 15% ; color: white; background:rgba(23, 25, 28, .9); border: 2px solid #5e676b; border-radius: 4px; height: 100px;text-align: center; padding-top: 50px;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ @import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700');
+</style>
+
+
+
+ </head>    
+      <body>
+     
+        <!--header}-->
+
+        <header >
+<p  class="logo">
+         <a href=""> <img src="/icon/image/logo.png"  alt="На главную" title="На главную"></a>
+
+</p>
+        </header>
+
+
+
+
+
+
+ <div class="user_menu">
+
+</div> 
+
+
+
+<div class="wrapper">
+ <nav class="search">
+       <input type="search" placeholder="Введите текст для поиска" size="21" maxlength="200" border="1" id="in" autofocus>
+
+                        
+                        
+
+                                            
+                   <div class="header"><a id="soc" href="/registr/login.php"  title="Войдите или зарегистрируйтесь на сайте">ВОЙДИТЕ</a></div>
+                   <div class="header"><a id="soc" href="#">VK</a></div>
+                   <div class="header"><a id="soc" href="#">TWITTER</a></div>
+                   <div class="header"><a id="soc" href="#">DISCORD</a> </div>
+                   <div class="header"><a id="soc" href="#">TELEGRAM</a></div>
+                   <div class="header" id="drop"> <a id="soc" href="#">ANIME</a> 
+                    <div class="dropcontent">
+<ul class="leftdropmenu">
+    <ul class="pick">
+<a href="">  <li id="w"> Связь с нами          </li></a>
+<a href="">  <li id="w"> Анонсы                </li></a>
+<a href="">  <li id="w"> Аниме новости         </li></a>
+<a href="">  <li id="w"> Коллекции             </li></a>
+<a href="">  <li id="w">Аниме Фильмы           </li></a>
+<a href="">  <li id="w">Популярные авторы      </li></a>
+<a href="">  <li id="w"> Лучшие студии(рисовки)</li></a>
+<a href="">  <li id="w">Разраб                 </li></a>
+</ul>
+
+<a href="" id="r">  <li > меха              </li></a>
+<a href="" id="r">  <li > сёнэн             </li></a>
+<a href="" id="r">  <li > драма             </li></a>
+<a href="" id="r">  <li > школа             </li></a>
+<a href="" id="r">  <li > война             </li></a>
+<a href="" id="r">  <li > сёдзё             </li></a>
+<a href="" id="r">  <li > ужасы             </li></a>
+<a href="" id="r">  <li > спорт             </li></a>
+<a href="" id="r">  <li > романтика         </li></a>
+<a href="" id="r">  <li > комедия           </li></a>
+<a href="" id="r">  <li > фантастика        </li></a>
+<a href="" id="r">  <li > фэнтези           </li></a>
+<a href="" id="r">  <li > повседневность    </li></a>
+<a href="" id="r">  <li > детектив          </li></a>
+<a href="" id="r">  <li > история           </li></a>
+<a href="" id="r">  <li > триллер           </li></a>
+<a href="" id="r">  <li > приключения       </li></a>
+<a href="" id="r">  <li > киберпанк         </li></a>
+<a href="" id="r">  <li > мистика           </li></a>
+<a href="" id="r">  <li > махо-сёдзё        </li></a>
+<a href="" id="r">  <li > сказка            </li></a>
+<a href="" id="r">  <li > сёдзё-ай          </li></a>
+<a href="" id="r">  <li > сёнэн-ай          </li></a>
+<a href="" id="r">  <li > боевые искусства  </li></a>
+<a href="" id="r">  <li > самураи           </li></a>
+<a href="" id="r">  <li > этти              </li></a>
+</ul>
+</div>
+</div>
+</nav> 
+
+<main class="otdel"><h2 id="h2"><hr>Выберите один из разделов нашего сайта!<hr></h2></main>
+<div class="double_wrapper">
+<p class="z1" ><a href="" id="c"> <span id="x">Аниме новости       </span></a></p>
+<p class="z2" ><a href="/anime/ориент.html" id="c"><span id="x">Смотреть аниме</span></a></p>
+<p class="z3" ><a href="" id="c"> <span id="x">Аниме фильмы        </span></a></p>
+<p class="z4" ><a href="" id="c"> <span id="x">Коллекции аниме     </span></a></p>
+<p class="z5" ><a href="" id="c"> <span id="x">Популярные авторы   </span></a></p>
+
+
+
+                            
+                               
+                               
+                         
+
+    </body>
+ </html>
+
